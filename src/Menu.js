bd971f1b536eb042bcb660c4c141346a5feb8d87
iamtanuj18 <iamtanuj18@yahoo.com>
2021-12-23 15:00:50 +0530

    third

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,6 @@ import React from "react";
 const Menu = ({ items }) => {
   return (
     <div className="section-center">
-<<<<<<< HEAD
       {items.map((menuItem) => {
         const { id, title, img, desc, price } = menuItem;
         return (
@@ -13,17 +12,6 @@ const Menu = ({ items }) => {
               <header>
                 <h4>{title}</h4>
                 <h4 className="price">${price}</h4>
-=======
-      {items.map((item) => {
-        const { id, desc, title, price, img } = item;
-        return (
-          <article key={id} className="menu-item">
-            <img className="photo" src={img} alt={title} />
-            <div className="item-info">
-              <header>
-                <h4>{title}</h4>
-                <h4 className="price">{price}</h4>
->>>>>>> 20585200b266c512590f9d3ba88e5abd004b4cc0
               </header>
               <p className="item-text">{desc}</p>
             </div>
