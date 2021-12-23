3b5a801f016675a983574e655f61476f2669ce9b
iamtanuj18 <iamtanuj18@yahoo.com>
2021-12-23 14:56:42 +0530

    second commit

--- src/Menu.js
+++ src/Menu.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-const Menu = ({ id, title, img, desc, price }) => {
+const Menu = ({ items }) => {
   return (
     <div className="section-center">
-      <article key={id}>
-        <img className="photo" src={img} alt={title} />
-        <div className="item-info">
-          <header>
-            <h4>{title}</h4>
-            <h4 className="price">{price}</h4>
-          </header>
-          <p>{desc}</p>
-        </div>
-      </article>
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
     </div>
   );
 };
